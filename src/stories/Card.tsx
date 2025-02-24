70aca70de26fad210ef1c0140f4d79849c865eb4
hieu <nguyentrunghieu17102k2@gmail.com>
2025-02-24 14:48:37 +0700

    a

--- src/stories/Card.tsx
+++ src/stories/Card.tsx
@@ -28,41 +28,47 @@ export const Card = ({ onClick, cursor, ...props }: CardProps) => {
     <div
       className={`card ${props.darkMode ? "card--dark" : "card--light"}`}
       style={{
-        display: "flex",
-        gap: "40px",
-        padding: "13px 44px 14px 15px",
         borderRadius: "24px",
         cursor: cursor || "default",
       }}
       onClick={onClick}
     >
-      <img
-        src={props.imageSrc}
-        alt={props.title}
-        className={!props.Islandscape ? "card__image" : "landscape__image"}
-      />
-      <div className={"card__content"}>
-        <h2 className="card__title">
-          {props.title.length > 40
-            ? props.title.slice(0, 35) + "..."
-            : props.title}
-        </h2>
-        <p
-          className={`card__description ${
-            props.truncateDescription ? "truncate" : ""
-          }`}
-        >
-          {props.description && (
-            <p style={{ color: "#7B7A81" }} className="card__description">
-              {props.description}
-            </p>
-          )}
-        </p>
-        <div style={{ color: "#424246" }} className="card__author">
-          {props.author}
-        </div>
-        <div style={{ color: "#7B7A81" }} className="card__date">
-          {props.date}
+      <div
+        className={"card__container"}
+        style={{
+          display: "flex",
+          gap: "40px",
+          borderRadius: "24px",
+        }}
+      >
+        <img
+          src={props.imageSrc}
+          alt={props.title}
+          className={!props.Islandscape ? "card__image" : "landscape__image"}
+        />
+        <div className={"card__content"}>
+          <h2 className="card__title">
+            {props.title.length > 40
+              ? props.title.slice(0, 35) + "..."
+              : props.title}
+          </h2>
+          <p
+            className={`card__description ${
+              props.truncateDescription ? "truncate" : ""
+            }`}
+          >
+            {props.description && (
+              <p style={{ color: "#7B7A81" }} className="card__description">
+                {props.description}
+              </p>
+            )}
+          </p>
+          <div style={{ color: "#424246" }} className="card__author">
+            {props.author}
+          </div>
+          <div style={{ color: "#7B7A81" }} className="card__date">
+            {props.date}
+          </div>
         </div>
       </div>
     </div>
